2fe1db4b997e4c29d3a2e834ee8333703a9ba058
Satchmo <louis.northmore@gmail.com>
2019-10-20 17:39:21 +0100

    API WIP

--- Server/Utils/HttpServer.cs
+++ Server/Utils/HttpServer.cs
@@ -3,8 +3,11 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Threading;
+using Server.MirDatabase;
+using Server.MirObjects;
 using Server.MirEnvir;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Server
 {
@@ -52,21 +55,15 @@ namespace Server
                     case "/stats":
                         
                         string servername = GameLanguage.GameName;
-                        string a = GameLanguage.OnlinePlayers.ToString();
-                        //string version = Server.Settings.
                         string pcount = SMain.Envir.Players.Count.ToString();
                         string mcount = SMain.Envir.MonsterCount.ToString();
                         string acount = SMain.Envir.AccountList.Count().ToString();
                         string ccount = SMain.Envir.CharacterList.Count.ToString();
                         string connections = SMain.Envir.Connections.Count.ToString();
 
-                        //string MirEnvir.
-
                         var stats = new
                         {
-                            a = a,
                             name = servername,
-                            //version = version,
                             accounts = acount,
                             characters = ccount,
                             players = pcount,
@@ -75,60 +72,132 @@ namespace Server
                         };
 
                         var json = JsonConvert.SerializeObject(stats);
-                        WriteResponse(response, json);
-                        break;
-
-                    case "/players":
-                        var players = SMain.Envir.Players;
-                        json = JsonConvert.SerializeObject(players, Formatting.Indented,
-                        new JsonSerializerSettings()
+                        try
+                        {
+                            json = JsonConvert.SerializeObject(stats, Formatting.Indented,
+                            new JsonSerializerSettings()
                             {
                                 ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
                             });
-                        WriteResponse(response, json);
+                            WriteResponse(response, json);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.Write(e);
+                        }
+                        break;
+
+                    case "/players":
+
+                        try
+                        {
+                            int playerscount = SMain.Envir.Players.Count;
+
+                            var players = new List<string>();
+                            players.Add("{ OnlinePlayers: " + playerscount + " }");
+
+                            if (playerscount > 0)
+                            {
+                                
+                                foreach (var player in SMain.Envir.Players)
+                                {
+
+                                    players.Add("{ name:" + player.Name + ", level:" + player.Level + ", class:" + player.Class + " }");
+                                   
+                                }
+
+                            }
+
+
+                            json = JsonConvert.SerializeObject(players, Formatting.Indented,
+                            new JsonSerializerSettings()
+                            {
+                                ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                            });
+                            WriteResponse(response, json);
+
+                        } catch (Exception e)
+                        {
+                            Console.Write(e);
+                        }
+                       
+
                         break;
 
                     case "/accounts":
                         var accounts = SMain.Envir.AccountList;
-                        json = JsonConvert.SerializeObject(accounts, Formatting.Indented,
-                        new JsonSerializerSettings()
+                        try
+                        {
+                            json = JsonConvert.SerializeObject(accounts, Formatting.Indented,
+                            new JsonSerializerSettings()
                             {
-                                   ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                                ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
                             });
-                        WriteResponse(response, json);
+                            WriteResponse(response, json);
+                        } catch (Exception e)
+                        {
+                            Console.Write(e);
+                        }
                         break;
 
                     case "/items":
                         var items = SMain.Envir.ItemInfoList;
-                        json = JsonConvert.SerializeObject(items, Formatting.Indented,
-                        new JsonSerializerSettings()
+                        try
+                        {
+                            json = JsonConvert.SerializeObject(items, Formatting.Indented,
+                            new JsonSerializerSettings()
                             {
-                                   ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                                ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
                             });
-                        WriteResponse(response, json);
+                            WriteResponse(response, json);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.Write(e);
+                        }
                         break;
 
-                    case "/maps":
-                        var maps = SMain.Envir.MapList;
-                        json = JsonConvert.SerializeObject(maps, Formatting.Indented,
-                        new JsonSerializerSettings()
+                    case "/mobs":
+                        var mobs = SMain.Envir.MonsterInfoList;
+                        try
                         {
-                            ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-                        });
-                        WriteResponse(response, json);
+                            json = JsonConvert.SerializeObject(mobs, Formatting.Indented,
+                            new JsonSerializerSettings()
+                            {
+                                ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                            });
+                            WriteResponse(response, json);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.Write(e);
+                        }
                         break;
 
                     case "/magic":
                         var magic = SMain.Envir.MagicInfoList;
-                        json = JsonConvert.SerializeObject(magic, Formatting.Indented,
-                        new JsonSerializerSettings()
+                        try
                         {
-                            ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-                        });
-                        WriteResponse(response, json);
+                            json = JsonConvert.SerializeObject(magic, Formatting.Indented,
+                            new JsonSerializerSettings()
+                            {
+                                ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                            });
+                            WriteResponse(response, json);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.Write(e);
+                        }
                         break;
 
                     case "/register":
+
+                        /*
+                         * eg:
+                         * http://localhost:5679/register?id=satch&psd=satch1&email=satch1@satch.com&name=satch1&question=1&answer=1&ip=1.1.1.1
+                         */
+
                         string id = request.QueryString["id"].ToString();
                         string psd = request.QueryString["psd"].ToString();
                         string email = request.QueryString["email"].ToString();
@@ -146,10 +215,19 @@ namespace Server
                         p.SecretQuestion = question;
                         p.SecretAnswer = answer;
                         int result = SMain.Envir.HTTPNewAccount(p,ip);
+                        
                         WriteResponse(response, result.ToString());
                         break;
 
                     case "/login":
+
+                        /*
+                         * Some of the Return codes
+                         * 7 = Success
+                         * 2 = Incorrect
+                         * 6 = too many attempts
+                         */
+
                         id = request.QueryString["id"].ToString();
                         psd = request.QueryString["psd"].ToString();
                         result = SMain.Envir.HTTPLogin(id, psd);
@@ -164,13 +242,13 @@ namespace Server
                         break;
 
                     default:
-                        WriteResponse(response, "error");
+                        WriteResponse(response, "No valid API endpoint specified");
                         break;
                 }
             }
             catch (Exception error)
             {
-                WriteResponse(response, "request error: " + error);
+                WriteResponse(response, "API Error: " + error);
             }
         }
 
